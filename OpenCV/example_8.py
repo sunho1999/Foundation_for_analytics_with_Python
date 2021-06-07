bc1b1527e481c38e7337ce695e9ce052a3295e34
sunho1999 <wmfrlek11@naver.com>
2021-06-07 20:22:05 +0900

    이미지 파일 추가

--- OpenCV/example_8.py
+++ OpenCV/example_8.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-image_1 = cv2.imread('images.jpg')
-image_2 = cv2.imread('cat.jpg')
+image_1 = cv2.imread('image_1.jpg')
+image_2 = cv2.imread('image_2.png')
 
 #이때 이미지 크기가 같아야함
 result = cv2.add(image_1, image_2)
---
+++ OpenCV/example_9.py
@@ -0,0 +1,3 @@
+import cv2
+
+#cv2.threshold(image,thresh,max_value,type)임계값을 기준으로 흑/백으로 분류하는 함수
